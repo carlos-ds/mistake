b57e099cc6f82c58f5dc9b1967073a8295771eb4
Karel De Smet <karel.de.smet@outlook.com>
2020-07-28 22:10:39 +0200

    Created a global ruleTypeOptions array to store the possible rule type values and enhanced the code to prepare for displaying the existing rules in storage on window load

--- options.js
+++ options.js
@@ -2,6 +2,9 @@ const buttonAddNewRule = document.getElementById("add");
 const rulesList = document.getElementById("rules");
 
 window.onload = function () {
+  const rules = getRules();
+  displayRules(rules);
+
   buttonAddNewRule.addEventListener("click", createRule);
   rulesList.addEventListener("click", saveRule);
   rulesList.addEventListener("click", removeRule);
@@ -20,14 +23,20 @@ function getRules() {
   }
 }
 
-function createRule() {
+function displayRules(rules) {
+  // for (const [key, value] of Object.entries(rules)) {
+  //   createRule(key
+  // }
+}
+
+function createRule(type) {
   const currentNumberOfRules = document.querySelectorAll(".rule").length;
 
   const newRule = document.createElement("div");
   newRule.classList.add("rule");
   newRule.setAttribute("data-index", currentNumberOfRules);
 
-  newRule.appendChild(createDropdown("rule type"));
+  newRule.appendChild(createDropdown("rule type", type));
   newRule.appendChild(createButton("save"));
   newRule.appendChild(createButton("remove"));
 
@@ -39,21 +48,27 @@ function saveRule(rule) {
     try {
       const ruleKey =
         "rule" + rule.target.parentNode.getAttribute("data-index");
+      const ruleType = rule.target.parentNode.querySelector(
+        '[data-dropdown-type="ruleType"]'
+      );
+
       chrome.storage.sync.set({
         [ruleKey]: {
-          key1: "value1",
-          key2: "value2",
+          // Set rule type as a value from the global ruleTypeOptions array
+          ruleType: ruleTypeOptions[ruleType.value],
         },
       });
       displayAlert("success", "The rule was successfully saved!");
     } catch (error) {
       displayAlert(
         "danger",
-        "The rule could not be removed. Please refresh the page and try again."
+        "The rule could not be saved. Please refresh the page and try again."
       );
     }
   }
 }
+
+// To clear all existing items from storage: chrome.storage.sync.clear()
 
 function removeRule(rule) {
   if (rule.target.getAttribute("data-action") === "remove") {
@@ -88,11 +103,23 @@ function createButton(type) {
   }
 }
 
-function createDropdown(type) {
+const ruleTypeOptions = ["URL begins with", "URL contains", "URL ends with"];
+
+function createDropdown(type, value) {
   if (type === "rule type") {
     const ruleTypeDropdown = document.createElement("select");
-    ruleTypeDropdown.innerHTML =
-      '<option value="1">Option 1</option><option value="2">Option 2</option>';
+    ruleTypeDropdown.setAttribute("data-dropdown-type", "ruleType");
+
+    // Create dropdown options based on global ruleTypeOptions array
+    for (i = 0; i < ruleTypeOptions.length; i++) {
+      const ruleTypeOption = document.createElement("option");
+      ruleTypeOption.setAttribute("value", i);
+      if (value === ruleTypeOptions[i]) {
+        ruleTypeOption.setAttribute("selected", "selected");
+      }
+      ruleTypeOption.innerText = ruleTypeOptions[i];
+      ruleTypeDropdown.appendChild(ruleTypeOption);
+    }
     return ruleTypeDropdown;
   }
 }
@@ -113,4 +140,7 @@ function displayAlert(type, text) {
     newAlert.classList.add("alert", "alert-danger");
   }
   document.body.prepend(newAlert);
+  setTimeout(function () {
+    newAlert.remove();
+  }, 2000);
 }
