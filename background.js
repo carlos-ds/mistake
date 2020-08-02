74b3fec46571d9f36de711291db4e50d7a2d4e52
Karel De Smet <karel.de.smet@outlook.com>
2020-08-02 21:50:45 +0200

    First attempt at communicating between background.js and the content script

--- background.js
+++ background.js
@@ -1,13 +1,18 @@
-chrome.runtime.onInstalled.addListener(function() {
-    console.log("Mistaken extension is installed!");
+chrome.runtime.onInstalled.addListener(function () {
+  console.log("Mistaken extension is installed!");
 });
 
 function logOnBefore(details) {
-    const standaardRegExp = /https:\/\/www.standaard\.be.*/;
-    if (details.url.match(standaardRegExp)) {
-        console.log("This is DS!");
-    }
+  const standaardRegExp = /https:\/\/www.standaard\.be.*/;
+  if (details.url.match(standaardRegExp)) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, { greeting: "hello" }, function (
+        response
+      ) {
+        console.log(response.farewell);
+      });
+    });
+  }
 }
 
 chrome.webNavigation.onBeforeNavigate.addListener(logOnBefore);
-
