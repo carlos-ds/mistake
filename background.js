f023a1ad8abd88a17a5c8f3db7dc131562262ebd
Karel De Smet <karel.de.smet@outlook.com>
2020-08-04 22:18:29 +0200

    Retrieve the values from sync in the content script and log them to the console if the expression matches the URL

--- background.js
+++ background.js
@@ -1,18 +1,3 @@
 chrome.runtime.onInstalled.addListener(function () {
   console.log("Mistaken extension is installed!");
 });
-
-function logOnBefore(details) {
-  const standaardRegExp = /https:\/\/www.standaard\.be.*/;
-  if (details.url.match(standaardRegExp)) {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { greeting: "hello" }, function (
-        response
-      ) {
-        console.log(response.farewell);
-      });
-    });
-  }
-}
-
-chrome.webNavigation.onBeforeNavigate.addListener(logOnBefore);
--- content.js
+++ content.js
@@ -1,1 +1,9 @@
-
+chrome.storage.sync.get(null, function (syncItems) {
+  for (const [key, value] of Object.entries(syncItems)) {
+    if (value.ruleExpression === window.location.href) {
+      // Display message here
+      console.log(key);
+      console.log(value);
+    }
+  }
+});
