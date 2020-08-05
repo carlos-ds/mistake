5e07b7c8a92c83c3cee54f381644c2f6bca8e6b0
Karel De Smet <karel.de.smet@outlook.com>
2020-08-05 21:55:26 +0200

    Draft of how to display a message on the url that matches the expression defined on the options page

--- content.js
+++ content.js
@@ -1,9 +1,29 @@
-chrome.storage.sync.get(null, function (syncItems) {
-  for (const [key, value] of Object.entries(syncItems)) {
-    if (value.ruleExpression === window.location.href) {
-      // Display message here
-      console.log(key);
-      console.log(value);
+window.onload = function () {
+  chrome.storage.sync.get(null, function (items) {
+    for (const value in Object.values(items)) {
+      if (
+        value.ruleType === "URL begins with" &&
+        urlBeginsWith(window.location.href, value.ruleExpression)
+      ) {
+        document.body.prepend(createMessage());
+      }
     }
-  }
-});
+  });
+};
+
+function urlBeginsWith(url, expression) {
+  const regularExpression = new RegExp(removeProtocol(expression) + ".*");
+  return removeProtocol(url).match(regularExpression);
+}
+
+// https://stackoverflow.com/questions/3999764/taking-off-the-http-or-https-off-a-javascript-string
+function removeProtocol(url) {
+  const urlNoProtocol = url.replace(/^https?\:\/\//i, "");
+  return urlNoProtocol;
+}
+
+function createMessage(text, color) {
+  const paragraph = document.createElement("p");
+  paragraph.innerText = "test";
+  return paragraph;
+}
